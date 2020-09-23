92b3ff77bf8d321ab505ce986e9ec11a43c2060e
Joel Barnes <j@barnzilla.ca>
2020-09-23 16:29:03 -0400

    Remove years in immigration data before 2000/2001

--- get-immigration-data.R
+++ get-immigration-data.R
@@ -1,8 +1,11 @@
 # Load packages to extend base R
-library(cansim)
+library(cansim); library(dplyr)
 
 # Get immigration data
 d <- get_cansim("17-10-0014-01")
+
+# Remove data prior to 2001
+d <- d %>% filter(! REF_DATE %in% unique(sort(d$REF_DATE))[1:29])
 
 # Create file path and file name
 file_path_and_file_name <- "c:/users/joelb/google drive/github/cansim/cansim-17-10-0014-01"
